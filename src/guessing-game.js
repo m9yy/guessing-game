b67c4403b83bd2ba1b8cce30adb02d9e39027f6d
Sergey Larchenko <newm9yy2020@gmail.com>
2021-09-05 12:53:26 +0300

    feat: ok

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,33 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.start = 0;
+        this.end = 0;
+        this.result = 0;
+
+        this.setRange = this.setRange.bind(this);
+        this.guess = this.guess.bind(this)
+        this.lower = this.lower.bind(this);
+        this.greater = this.greater.bind(this);
+    }
 
     setRange(min, max) {
-
+        this.start = min
+        this.end = max
     }
 
     guess() {
-
+        this.result =Math.round ((this.start + this.end) / 2);
+        return this.result;
     }
 
     lower() {
-
+        this.end = this.result
+        return this.end
     }
 
     greater() {
-
+        this.start = this.result
+        return this.start
     }
 }
 
